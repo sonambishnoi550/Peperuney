07f4d75c61a2a825201b7137b0a4142561220ecd
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-03-07 20:42:06 +0530

    add meta-tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,17 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Peperuney",
   description: "The pizza-slingin’, joint-tokin’, all-in degen fresh from the Matt Furie multiverse, here to leave his greasy mark on the memeverse one slice at a time",
+  openGraph: {
+    title: "Peperuney",
+    description: "The pizza-slingin’, joint-tokin’, all-in degen fresh from the Matt Furie multiverse, here to leave his greasy mark on the memeverse one slice at a time",
+    images: [
+      {
+        url: "/meta-tag.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
