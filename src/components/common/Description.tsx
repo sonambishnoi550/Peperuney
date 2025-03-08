85fd49c0553335dfe8b931464dc9da27c6a06702
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-03-08 09:40:19 +0530

    same

--- src/components/common/Description.tsx
+++ src/components/common/Description.tsx
@@ -5,5 +5,4 @@ const Description = ({ MyText="", MyClass="" }) => {
         <p className={`font-normal text-xl leading-7 sm:text-[22px] sm:leading-9 xl:leading-[43.2px] xl:text-2xl ff-schoolbell text-[#0E0E0E] ${MyClass}`}>{MyText}</p>
     )
 }
-
 export default Description
